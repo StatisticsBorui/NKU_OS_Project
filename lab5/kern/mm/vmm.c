d365564efcea6c292f9ac74aec563cfd00307143
StatisticsBorui <540166419@qq.com>
2024-12-19 16:24:22 -0500

    lab5 exercise commit --Borui

--- lab5/kern/mm/vmm.c
+++ lab5/kern/mm/vmm.c
@@ -435,7 +435,7 @@ do_pgfault(struct mm_struct *mm, uint_t error_code, uintptr_t addr) {
             goto failed;
         }
     } else {
-        /*LAB3 EXERCISE 3: YOUR CODE
+        /*LAB3 EXERCISE 3: 2111511
         * 请你根据以下信息提示，补充函数
         * 现在我们认为pte是一个交换条目，那我们应该从磁盘加载数据并放到带有phy addr的页面，
         * 并将phy addr与逻辑addr映射，触发交换管理器记录该页面的访问情况
@@ -447,6 +447,7 @@ do_pgfault(struct mm_struct *mm, uint_t error_code, uintptr_t addr) {
         *    page_insert ： 建立一个Page的phy addr与线性addr la的映射
         *    swap_map_swappable ： 设置页面可交换
         */
+       
         if (swap_init_ok) {
             struct Page *page = NULL;
             // 你要编写的内容在这里，请基于上文说明以及下文的英文注释完成代码编写
@@ -458,6 +459,17 @@ do_pgfault(struct mm_struct *mm, uint_t error_code, uintptr_t addr) {
             //map of phy addr <--->
             //logical addr
             //(3) make the page swappable.
+
+            if((ret = swap_in(mm, addr, &page)) != 0){
+                cprintf("swap_in failed\n");
+                goto failed;
+            }
+            if((ret = page_insert(mm -> pgdir, page, addr, perm)) != 0){
+                cprintf("page_insert failed\n");
+                goto failed;
+            }
+            swap_map_swappable(mm, addr, page, 1);
+
             page->pra_vaddr = addr;
         } else {
             cprintf("no swap_init_ok but ptep is %x, failed\n", *ptep);
--- lab5/kern/process/proc.c
+++ lab5/kern/process/proc.c
@@ -87,7 +87,7 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
-    //LAB4:EXERCISE1 YOUR CODE
+    //LAB4:EXERCISE1 2111511
     /*
      * below fields in proc_struct need to be initialized
      *       enum proc_state state;                      // Process state
@@ -103,13 +103,30 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-
-     //LAB5 YOUR CODE : (update LAB4 steps)
+        proc -> state = PROC_UNINIT;
+        proc -> pid = -1;
+        proc -> runs = 0;
+        proc -> kstack = 0;
+        proc -> need_resched = 0;
+        proc -> parent = NULL;
+        proc -> mm = NULL;
+        memset(&(proc -> context), 0, sizeof(struct context));
+        proc -> tf = NULL;
+        proc -> cr3 = boot_cr3;
+        proc -> flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN);
+     //LAB5 YOUR CODE : 2111511
      /*
      * below fields(add in LAB5) in proc_struct need to be initialized  
      *       uint32_t wait_state;                        // waiting state
      *       struct proc_struct *cptr, *yptr, *optr;     // relations between processes
      */
+
+        proc -> wait_state = 0;
+        proc -> cptr = NULL;
+        proc -> yptr = NULL;
+        proc -> optr = NULL;
+
     }
     return proc;
 }
@@ -197,7 +214,7 @@ get_pid(void) {
 void
 proc_run(struct proc_struct *proc) {
     if (proc != current) {
-        // LAB4:EXERCISE3 YOUR CODE
+        // LAB4:EXERCISE3 2111511
         /*
         * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
         * MACROs or Functions:
@@ -206,7 +223,15 @@ proc_run(struct proc_struct *proc) {
         *   lcr3():                   Modify the value of CR3 register
         *   switch_to():              Context switching between two processes
         */
-
+        int flag;
+        struct proc_struct *temp = current;
+        local_intr_save(flag);
+        {
+            current = proc;
+            lcr3(proc -> cr3);
+            switch_to(&(temp -> context), &(proc -> context));
+        }
+        local_intr_restore(flag);
     }
 }
 
@@ -369,7 +394,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:EXERCISE2 2111511
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -395,7 +420,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
 
-    //LAB5 YOUR CODE : (update LAB4 steps)
+    //LAB5 YOUR CODE : 2111511
     //TIPS: you should modify your written code in lab4(step1 and step5), not add more code.
    /* Some Functions
     *    set_links:  set the relation links of process.  ALSO SEE: remove_links:  lean the relation links of process 
@@ -403,6 +428,38 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     *    update step 1: set child proc's parent to current process, make sure current process's wait_state is 0
     *    update step 5: insert proc_struct into hash_list && proc_list, set the relation links of process
     */
+    if ((proc = alloc_proc()) == NULL) {
+        goto fork_out;
+    }
+    proc -> parent = current;
+    if (current -> wait_state != 0){
+        goto fork_out;
+    }
+    //    2. call setup_kstack to allocate a kernel stack for child process
+    if(setup_kstack(proc) != 0){
+        goto bad_fork_cleanup_proc;
+    }
+    //    3. call copy_mm to dup OR share mm according clone_flag
+    if(copy_mm(clone_flags, proc) != 0){
+        goto bad_fork_cleanup_kstack;
+    }
+    //    4. call copy_thread to setup tf & context in proc_struct
+    copy_thread(proc, stack, tf);
+    //    5. insert proc_struct into hash_list && proc_list
+    int flag;
+    local_intr_save(flag);
+    {
+    proc -> pid = get_pid();
+    hash_proc(proc);
+    //list_add(&proc_list, &(proc -> list_link));
+    //nr_process++;
+    set_links(proc);
+    }
+    local_intr_restore(flag);
+    //    6. call wakeup_proc to make the new child process RUNNABLE
+    wakeup_proc(proc);
+    //    7. set ret vaule using child proc's pid
+    ret = proc -> pid;
  
 fork_out:
     return ret;
@@ -595,7 +652,7 @@ load_icode(unsigned char *binary, size_t size) {
     // Keep sstatus
     uintptr_t sstatus = tf->status;
     memset(tf, 0, sizeof(struct trapframe));
-    /* LAB5:EXERCISE1 YOUR CODE
+    /* LAB5:EXERCISE1 2111511
      * should set tf->gpr.sp, tf->epc, tf->status
      * NOTICE: If we set trapframe correctly, then the user level process can return to USER MODE from kernel. So
      *          tf->gpr.sp should be user stack top (the value of sp)
@@ -603,6 +660,11 @@ load_icode(unsigned char *binary, size_t size) {
      *          tf->status should be appropriate for user program (the value of sstatus)
      *          hint: check meaning of SPP, SPIE in SSTATUS, use them by SSTATUS_SPP, SSTATUS_SPIE(defined in risv.h)
      */
+
+    tf -> gpr.sp = USTACKTOP;
+    tf -> epc = elf -> e_entry;
+    tf -> status = (read_csr(sstatus) & ~SSTATUS_SPP) | SSTATUS_SPIE;
+
 
 
     ret = 0;
